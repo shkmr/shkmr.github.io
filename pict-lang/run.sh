7525f35ff8e3f566635eb3ac77111979552174e8
Shigenobu Kimura <skimu@mac.com>
2015-11-28 21:39:36 -0500

    make pa.gif and pb.gif infinite loop

--- pict-lang/run.sh
+++ pict-lang/run.sh
@@ -1,7 +1,7 @@
 rm -rf pa pb
 mkdir -p pa pb
 gosh -l./pict-core.scm parallel.scm
-convert -loop 1 pa/*[16].png pa.gif
-convert -loop 1 pb/*[16].png pb.gif
+convert -loop 0 pa/*[16].png pa.gif
+convert -loop 0 pb/*[16].png pb.gif
 
 gosh -l./pict-core.scm fractal.scm
