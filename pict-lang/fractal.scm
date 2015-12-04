4ae5d76fe2040ea7a0f68f437de2377c63774ee2
Shigenobu Kimura <skimu@mac.com>
2015-12-03 22:17:28 -0600

    correct fractal transform and make new anime

--- pict-lang/fractal.scm
+++ pict-lang/fractal.scm
@@ -1,4 +1,5 @@
 (use gauche.time)
+(use srfi-1)
 (use ggc.skimu.frame-buffer)
 
 (define (draw-line fb p1 p2)
@@ -24,13 +25,20 @@
     (save-frame-buffer-as-png-file fb filename)
     ))
 
-(define dragon (dragon point 10))
-(define koch   (koch   simple-segment 9))
-(define levy   (levy   point 12))
+(define (run n)
+  (let ((d (dragon simple-segment n))
+        (k (koch   simple-segment n))
+        (l (levy   simple-segment n)))
+    (print n)
+    (draw-painter (shift (scale d 0.6) (make-vect 0.25 0.60)) 400 400 (format #f "dragon~2,'0d.png" n))
+    (draw-painter (shift k             (make-vect 0.00 0.50)) 400 400 (format #f "koch~2,'0d.png" n))
+    (draw-painter (shift (scale l 0.5) (make-vect 0.25 0.65)) 400 400 (format #f "levy~2,'0d.png" n)))
+    (sys-system "convert -loop 15 dragon*.png dragon.gif")
+    (sys-system "convert -loop 15 koch*.png   koch.gif")
+    (sys-system "convert -loop 15 levy*.png   levy.gif")
+  )
 
-(time (draw-painter (shift (scale dragon 0.5) (make-vect 0.25 0.50)) 400 400 "dragon.png"))
-(time (draw-painter (shift koch               (make-vect 0.00 0.50)) 400 400 "koch.png"))
-(time (draw-painter (shift (scale levy   0.6) (make-vect 0.12 0.45)) 400 400 "levy.png"))
+(for-each run (iota 15))
 
 (exit 0)
 ;;;
